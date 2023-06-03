d8f7f08859ad295be41698d4d91f7c3a57fdf779
Sameera Prasantha <94036083+sameerao1996@users.noreply.github.com>
2023-06-03 13:56:45 +0530

    change and create MySQL database importer
    All Source file uploaded

---
+++ Sql Database importer/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace Sql_Database_import_and_export
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(281, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tool For SQL Database Importer";
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(12, 158);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(114, 27);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "Submit";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(156, 62);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(640, 376);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(12, 103);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(114, 25);
+            this.btn1.TabIndex = 4;
+            this.btn1.Text = "Attached file";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(739, 32);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(57, 24);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 428);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Version 1.2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(808, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " MySQL Database Importer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Sql Database importer/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using MySql.Data.MySqlClient;
+
+namespace Sql_Database_import_and_export
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text == "")
+            {
+                MessageBox.Show("Please attached the SQL file", "Warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                try
+                {
+                    string con = "datasource=localhost;port=3306;username=root;password=''";
+                    string Query = richTextBox1.Text;
+                    MySqlConnection MyConn2 = new MySqlConnection(con);
+                    MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+                    MySqlDataReader MyReader2;
+                    MyConn2.Open();
+                    MyReader2 = MyCommand2.ExecuteReader();     // Here our query will be executed and data saved into the database.
+                    MessageBox.Show("The database addition was successful.", "Infomation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    while (MyReader2.Read())
+                    {
+                    }
+                    MyConn2.Close();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("This database has already been added.", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            Stream st;
+            OpenFileDialog dr = new OpenFileDialog();
+            dr.Filter = "SQL files (*.sql)|*.sql";
+            if (dr.ShowDialog() == DialogResult.OK)
+            {
+                string file = dr.FileName;
+                if ((st = dr.OpenFile()) != null && Path.GetExtension(file).Equals(".sql", StringComparison.OrdinalIgnoreCase))
+                {
+                    string str = File.ReadAllText(file);
+                    richTextBox1.Text = str;
+                }
+            }
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string con = "datasource=localhost;port=3306;username=root;password=''";
+                MySqlConnection MyConn2 = new MySqlConnection(con);
+                MyConn2.Open();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please start the MySQL server. Please restart this application if you are running the server", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                btn1.Enabled = false;
+                btn2.Enabled = false;
+                btnClear.Enabled = false;
+                richTextBox1.Enabled = false;
+            }
+
+
+        }
+    }
+}
